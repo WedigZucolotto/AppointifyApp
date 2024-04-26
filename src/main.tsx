53686df6b0feac84e61f498bb1370c3f4766b375
Gabriel Garcia Wedig <gabriel.wedig@cwi.com.br>
2024-04-26 16:05:32 -0300

    WiP

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { GlobalContextProvider, SnackbarContextProvider } from './hooks'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import AuthOutlet from '@auth-kit/react-router/AuthOutlet'
+import AuthProvider from 'react-auth-kit'
+import createStore from 'react-auth-kit/createStore'
 import {
   Company,
   Day,
@@ -14,47 +17,63 @@ import {
   User,
   Week,
   Event,
-  Schedule
+  Schedule,
+  Login
 } from './pages'
-import { Login } from './pages/login/Login'
 
 const element = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(element)
 
+const store = createStore<{}>({
+  authName: '_auth',
+  authType: 'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: false
+})
+
+const PublicRoutes = () => (
+  <Routes>
+    <Route path="*" element={<NoPage />} />
+    <Route path="/" element={<Navigate to="/login" />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/:id/schedule" element={<Schedule />} />
+  </Routes>
+)
+
+const PrivateRoutes = () => (
+  <Routes>
+    <Route element={<AuthOutlet fallbackPath="/login" />}>
+      <Route
+        path="/management"
+        element={<Navigate to="/management/companies" />}
+      />
+      <Route path="/management/companies" element={<Company />} />
+      <Route path="/management/companies/:id/users" element={<User />} />
+      <Route path="/management/companies/:id/services" element={<Service />} />
+      <Route path="/management/companies/:id/events" element={<Event />} />
+      <Route path="/management/events" element={<Event />} />
+      <Route path="/management/plans" element={<Plan />} />
+      <Route path="/management/services" element={<Service />} />
+      <Route path="/management/users" element={<User />} />
+      <Route path="/day" element={<Day />} />
+      <Route path="/week" element={<Week />} />
+      <Route path="/month" element={<Month />} />
+    </Route>
+  </Routes>
+)
+
 root.render(
   <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <SnackbarContextProvider>
-      <GlobalContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/schedule" />} />
-            <Route
-              path="/management"
-              element={<Navigate to="/management/companies" />}
-            />
-            <Route path="/management/companies" element={<Company />} />
-            <Route path="/management/companies/:id/users" element={<User />} />
-            <Route
-              path="/management/companies/:companyId/services"
-              element={<Service />}
-            />
-            <Route
-              path="/management/companies/:id/events"
-              element={<Event />}
-            />
-            <Route path="/management/events" element={<Event />} />
-            <Route path="/management/plans" element={<Plan />} />
-            <Route path="/management/services" element={<Service />} />
-            <Route path="/management/users" element={<User />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/day" element={<Day />} />
-            <Route path="/week" element={<Week />} />
-            <Route path="/month" element={<Month />} />
-          </Routes>
-        </BrowserRouter>
-      </GlobalContextProvider>
-      <Snackbar />
-    </SnackbarContextProvider>
+    <AuthProvider store={store}>
+      <SnackbarContextProvider>
+        <GlobalContextProvider>
+          <BrowserRouter>
+            <PublicRoutes />
+            <PrivateRoutes />
+          </BrowserRouter>
+        </GlobalContextProvider>
+        <Snackbar />
+      </SnackbarContextProvider>
+    </AuthProvider>
   </LocalizationProvider>
 )
---
+++ src/pages/schedule/schema.ts
@@ -0,0 +1,24 @@
+import * as yup from 'yup'
+
+const phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/
+
+const schema = {
+  name: yup.string().required('Campo obrigatório'),
+  lastname: yup.string().required('Campo obrigatório'),
+  contact: yup
+    .string()
+    .matches(phoneRegex, 'Número de telefone inválido')
+    .required('Campo obrigatório'),
+  service: yup.string().required('Campo obrigatório'),
+  date: yup.string().required('Campo obrigatório')
+  // hour: yup.string().required('Campo obrigatório')
+}
+
+const extraSchema = {
+  ...schema,
+  local: yup.string().required('Campo obrigatório'),
+  employee: yup.string().required('Campo obrigatório')
+}
+
+export const getScheduleSchema = (showExtraFields?: boolean) =>
+  yup.object().shape(showExtraFields ? extraSchema : schema)
