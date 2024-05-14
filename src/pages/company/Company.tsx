4afb138534d7ab602f1b5cf5e287c35ee18e38e0
DouglasZucolotto <douglaszuco2@gmail.com>
2024-05-14 17:11:39 -0300

    ✨ company tab

--- src/pages/company/Company.tsx
+++ src/pages/company/Company.tsx
@@ -1,31 +1,78 @@
-import { TimePicker } from '@mui/x-date-pickers'
-import { Button, Header, TextInput } from '../../components'
+import { useEffect, useState } from 'react'
+import { Button, Header, TextInput, TimeInput } from '../../components'
 import * as S from './style'
 import { FieldValues, useForm } from 'react-hook-form'
+import { UpdateCompanyRequest, useCompanies, useTryCatch } from '../../hooks'
+import { useParams } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+interface FormData {
+  name: string
+  open: string
+  close: string
+}
 
 export const Company = () => {
-  const { control, handleSubmit } = useForm()
+  const { callApi, fetchWithMessage } = useTryCatch()
+  const { getCompanyById, updateCompany } = useCompanies()
+
+  const { companyId = '' } = useParams()
+
+  useEffect(() => {
+    fetchCompany()
+  }, [])
+
+  const fetchCompany = async () => {
+    const { data, success } = await callApi(getCompanyById(companyId))
+
+    if (data && success) {
+      reset({
+        name: data.name,
+        // open: data.open,
+        // close: data.close
+      })
+    }
+  }
+
+  const schema = yup.object().shape({
+    name: yup.string().required('Campo obrigatório'),
+    open: yup.string().required('Campo obrigatório'),
+    close: yup.string().required('Campo obrigatório')
+  })
+
+  const { control, handleSubmit, reset } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
 
   const handleFormSubmit = async (values: FieldValues) => {
-    console.log(values)
+    await fetchWithMessage(
+      updateCompany(companyId, values as UpdateCompanyRequest),
+      'Editado com sucesso!'
+    )
+    fetchCompany()
   }
 
   return (
     <>
       <Header showCalendarFields={false} />
       <S.Company>
-        <S.Title>Douglax e Gaybriel</S.Title>
+        <S.Title>Editar empresa</S.Title>
         <TextInput
           label="Nome da Empresa"
           placeholder="ex: Wedig & Zucolotto"
           name="name"
           control={control}
         />
-        <S.Time>
-          <TimePicker ampm={false} name="open" />
-          <TimePicker ampm={false} name="close" />
-        </S.Time>
-        <Button type='schedule' onClick={handleSubmit(handleFormSubmit)}>Enviar</Button>
+        <TimeInput control={control} label="Horário de Abertura" name="open" />
+        <TimeInput
+          control={control}
+          label="Horário de fechamento"
+          name="close"
+        />
+        <Button type="schedule" onClick={handleSubmit(handleFormSubmit)}>
+          Enviar
+        </Button>
       </S.Company>
     </>
   )
