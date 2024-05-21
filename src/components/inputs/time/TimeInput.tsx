e0437f3f41261c17e7b102248b97cb532d2b9194
Gabriel Garcia Wedig <gabriel.wedig@cwi.com.br>
2024-05-20 23:38:20 -0300

    WiP

--- src/components/inputs/time/TimeInput.tsx
+++ src/components/inputs/time/TimeInput.tsx
@@ -2,12 +2,13 @@ import * as S from './style'
 import { Control, FieldValues, Path, useController } from 'react-hook-form'
 import { InputError } from '../error/InputError'
 import { TimePicker } from '@mui/x-date-pickers'
+import dayjs, { Dayjs } from 'dayjs'
 
 interface TimeInputProps<TFieldValues extends FieldValues> {
   name: Path<TFieldValues>
   control: Control<TFieldValues>
   label: string
-  //onChange?: (value: string) => void
+  onChange?: (value: Dayjs | null) => void
   disabled?: boolean
 }
 
@@ -15,18 +16,18 @@ export function TimeInput<TFieldValues extends FieldValues = FieldValues>({
   name,
   control,
   label,
-  //onChange,
+  onChange,
   disabled
 }: TimeInputProps<TFieldValues>) {
   const { fieldState, field } = useController({ name, control })
 
-  //   const changeEventHandler = (date: Dayjs | null) => {
-  //     if (date) {
-  //       const dateString = date.format('DD/MM/YYYY')
-  //       field.onChange(dateString)
-  //       onChange?.(dateString)
-  //     }
-  //   }
+  const changeEventHandler = (date: Dayjs | null) => {
+    try {
+      onChange?.call(null, date)
+    } finally {
+      field.onChange(date)
+    }
+  }
 
   return (
     <S.TimeInput>
@@ -36,6 +37,8 @@ export function TimeInput<TFieldValues extends FieldValues = FieldValues>({
         {...field}
         ampm={false}
         disabled={disabled}
+        onChange={changeEventHandler}
+        value={field.value ?? dayjs().startOf('day')}
       />
       <InputError message={fieldState.error?.message} />
     </S.TimeInput>
